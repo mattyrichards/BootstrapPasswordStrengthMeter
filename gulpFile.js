c4407116de0879fa45951b2fa2451eb1261e0286
Matt Richards <me@mttrchrds.com>
2024-03-02 19:32:33 +0000

    Minor updates to get this old repo working.

--- gulpFile.js
+++ gulpFile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename');
 
-gulp.task('build-js', function() {
+gulp.task('build-js', async function() {
   gulp.src('src/*.js')
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify({preserveComments: "license"}))
